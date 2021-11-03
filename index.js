245fb5d411f4473822c64b269b46babaf7e296f9
kevnjs <91511588+kevnjs@users.noreply.github.com>
2021-11-03 12:43:38 -0500

    finished questions except 6

--- index.js
+++ index.js
@@ -45,8 +45,10 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(array){
+  const arrayCopy = Array.from(array);
+    return arrayCopy;
+
 }    
 
 
@@ -64,8 +66,9 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
- /*your code here*/
+function is31Flavors(array){
+  const arraylength = array.length;
+  return (array.length === 31 ? true : false);
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -81,8 +84,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
- /*your code here*/
+function addFlavor(array, flavor){
+  const newFlavor = array.unshift(flavor);
+  return array;
 }
 
 
@@ -97,8 +101,9 @@ Use the removeLastFlavor function below to do the following:
   For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+function removeLastFlavor(array){
+  const removeFlavor = array.pop()
+  return array;
 }
 
 
@@ -115,7 +120,7 @@ Use the getFlavorByIndex function below to do the following:
 */
 
 function getFlavorByIndex(/*your code here*/){
-  /*your code here*/
+  return array[index];
 }
 
 
@@ -134,8 +139,9 @@ Use the removeFlavorByName function below to do the following:
   HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-  /*your code here*/
+function removeFlavorByName(array, flavor){
+  array.splice(flavor, 1);
+  return array;
 }
 
 
@@ -160,8 +166,14 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(array, string){
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].includes(string)) {
+      const newArray = [];
+      newArray.push(array[i]);
+      return newArray;
+    }
+  }
 }
 
 
